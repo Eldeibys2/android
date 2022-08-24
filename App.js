adc70eaf2588dbef8dffcdc9562960263f6ce282
203 <203@academicos.cesde.edu.co>
2022-08-24 08:52:18 -0500

    Maquetacion react native

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text,View } from 'react-native';
+//import { styles1 } from './assets/Mystyles';
 
-export default function App() {
+
+ //const App =() => {
+  export default function App() {
+    
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles1.container,{borderRadius:10,flexDirection:'colum'}]}>
+      <View style={[viewChilds.view,{flex:1,backgroundColor:'yellow'}]}>
+        <Text> Bannner</Text>
+        
+      </View>
+
+         <View style={[viewChilds.view,{flex:1,backgroundColor:'yellowgreen'}]}>
+
+          <Text>
+            Barra de navegacion
+          </Text>
+
+         </View> 
+
+         <View style={[viewChilds.view,{flex:5,backgroundColor:'gray',}]}>
+
+          <Text>
+            Cuerpo
+          </Text>
+
+         </View> 
+         <View style={[viewChilds.view,{flex:1,backgroundColor:'pink',}]}>
+
+          <Text>
+            Barra de navegacion
+          </Text>
+
+         </View> 
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+const styles1 = StyleSheet.create({
+container: {
+  flex: 2,
+  backgroundColor: 'green',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+Texts:{
+  fontSize:18,  
+  fontWeight:'bold',
+  color:'red'
+}
 });
+
+const viewChilds = StyleSheet.create({
+  view:{
+    width:'80%',
+    marginLeft:'5%',
+    marginTop:10
+  }
+})
+//export default App
+
---
+++ assets/Mystyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet} from 'react-native';
+
+const styles1 = StyleSheet.create({
+    container: {
+      flex: 2,
+      backgroundColor: 'green',
+     alignItems: 'center',
+      justifyContent: 'center',
+    },
+    Texts:{
+      fontSize:18,  
+      fontWeight:'bold',
+      color:'red'
+    }
+});
+
+export {styles1}
